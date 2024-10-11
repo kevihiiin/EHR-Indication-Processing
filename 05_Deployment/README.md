a25489ee4c16ef98a22ddbc219a7ae7f16f7f00d
Kevin Yuan <kevihiiin@users.noreply.github.com>
2024-10-11 16:16:40 +0100

    Refactor inferrence script and add instructions

--- 05_Deployment/README.md
+++ 05_Deployment/README.md
@@ -1,11 +1,78 @@
 # Deployment of the model
 Pre-trained model ready to be used in an production environment.
 
-Three options are available: 
-1. Run via standalone Python CLI
+Two options are available: 
+1. Run via standalone Python version via CLI
 2. Standalone deployment via Docker (recommended)
-3. Integrate into project
 
-## Standalone Python
+## Standalone Python/Bash
+The model can be run via the CLI. The following steps are required to run the model:
+1. Install the required packages via `pip install -r requirements_deployment.txt`
+2. Download the pretrained model weights into a path of your choice (`<path_to_model>`)
+
+```
+Classify Free Text Indications
+
+optional arguments:
+  -h, --help            show this help message and exit
+  
+  --input_file INPUT_FILE
+                        Path to the input files
+                        
+  --output_file OUTPUT_FILE
+                        Path to save the output results
+                        
+  --model_path MODEL_PATH
+                        Path to the directory containing the model and tokenizer
+                        
+  --indication_column INDICATION_COLUMN
+                        Name of the column in the input file containing the indications
+                        
+  --unique_output       Returns the result for just the distinct subset of indications.
+  
+  --device DEVICE       Device to run the model on ('cpu' or '0', '1',... for GPU)
+  
+  --batch_size BATCH_SIZE
+                        Batch size for inference
+                        
+  --pred_threshold PRED_THRESHOLD
+                        Threshold for binarizing the predictions
+```
+
+**Basic Usage Example:**
+```bash
+python3 inferrence.py --input_file data/abx_prescriptions.csv --output_file data/abx_prescriptions_classified.csv --model_path data/Bio_ClinicalBERT_5615.pth
+
+```
+-> This will return the original input dataframe with added columns for the model predictions.
+
+**Indication Column**
+
+By default, the model will look for the column named `Indication` in the input file.
+If you have a different column name, use the `--indication_column` flag to specify the column name.
+
+```bash
+python3 inferrence.py --input_file <input_file> --output_file <output_file> --model_path <model_path> --indication_column "My Column"
+```
+
+**Unique Output**
+
+To return the result for just the distinct subset of indications, use the `--unique_output` flag. 
+This will return only the indications column + the model predictions.
+
+```bash
+python3 inferrence.py --input_file <input_file> --output_file <output_file> --model_path <model_path> --unique_output
+```
+
+**Usage GPU or MPS**
+
+By default the model will run on the CPU. To run the model on the GPU, use the `--device` flag. 
+This is passed on to pytorch as the device to use.
+For a GPU, the device should be set to the GPU number, on Apple Silicon, this should be set to `mps`
+For example, to run the model on GPU 0, use the following command:
+```bash
+python3 inferrence.py --input_file <input_file> --output_file <output_file> --model_path <model_path> --device 0
+```
 
 ## Standalone Docker
+TODO, add docker wrapper for the model
--- 05_Deployment/inferrence.py
+++ 05_Deployment/inferrence.py
@@ -4,11 +4,14 @@ This tool will perform the following actions given a collection of indications
 1. Convert the indications to lowercase and create a de-duplicated copy
 2. Perform classification (inference)
 3. Expand back to the original size and order
+
+Usage:
+    python inferrence.py --input_file <path_to_input_file> --output_path <path_to_output_dir> --model_path <path_to_model_dir>
+
 """
 
 import pandas as pd
-import torch
-
+import argparse
 from pathlib import Path
 from tqdm.auto import tqdm
 
@@ -17,67 +20,94 @@ from datasets import Dataset
 from transformers import AutoTokenizer, pipeline, AutoModelForSequenceClassification
 from transformers.pipelines.pt_utils import KeyDataset
 
-# --- Configuration ---
-base_data_path = Path("/home/kevin/DPhil/Projects/EHR-Indication-Processing/98_Testing")  # Path to the model & data
-saved_model_name = "Bio_ClinicalBERT_4346.pth"  # Saved model name
-model_hf_id = "emilyalsentzer/Bio_ClinicalBERT"  # Currently required for the correct tokenizer
-cuda_device = "cpu"  # Change to "0" for the first GPU, or "cpu" for CPU
 
-batch_size = 8  # Batch size for inference, we used 8 for the training of this model, keep it at 8
-pred_threshold = 0.5  # Threshold for binarising the predictions, we choose 0.5 for training, can be changed
+def main(args):
+    # --- Configuration ---
+    input_file = Path(args.input_file)
+    indication_column = args.indication_column
+    output_file = Path(args.output_file)
+    model_path = Path(args.model_path)
 
-# --- Import Data ---
-# Import the data
-inference_input_df = pd.read_csv(base_data_path / "test_data_2000.csv", dtype={"Indication": str}) \
-    .dropna()
-# Pick the column of interest
-inference_indication_cased = inference_input_df.Indication
+    # --- Import Data ---
+    print(f"Reading input file from {input_file}")
+    input_df = (
+        pd.read_csv(
+            input_file,
+            dtype={indication_column: str},
+        )
+    )
 
-# --- Preprocess Data ---
-# Convert to lower case and make unique
-inference_df = pd.DataFrame({"Input_String": inference_indication_cased.str.lower().drop_duplicates()})
-# Create a Huggingface Dataset
-inference_dataset = Dataset.from_pandas(inference_df)
-print(f"Inference dataset size: {len(inference_dataset)} unique entries from {len(inference_indication_cased)} total")
+    # --- Unique Output ---
+    if args.unique_output:
+        input_df = (input_df
+                    .loc[:, [indication_column]]
+                    .drop_duplicates()
+        )
 
-# --- Loading Model & Inference ---
-# Load the model and tokeniser
-model = AutoModelForSequenceClassification.from_pretrained(base_data_path / saved_model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_hf_id)
-# Set up the pipeline
-inference_pipeline = pipeline("text-classification", model=model, tokenizer=tokenizer, top_k=None, device=cuda_device)
+    # --- Preprocess Data ---
+    # Convert to lower case, as the model was trained on uncased data
+    input_df[f"_Input_String"] = input_df[indication_column].str.lower()
 
-# Classify the infections, runs in batches
-pred_output = []
-for out in tqdm(
-        inference_pipeline(
-            KeyDataset(inference_dataset, "Input_String"),
-            batch_size=batch_size)
-):
-    pred_output.append(out)
+    # Create the inference dataset (HF Dataset) with unique entries and no NAs
+    inference_dataset = Dataset.from_pandas(
+        input_df
+        .loc[:, ["_Input_String"]]
+        .drop_duplicates()
+        .dropna()
+    )
+    print(f"Inference dataset size: {len(inference_dataset)} unique entries from {len(input_df)} total")
 
-# --- Postprocess Data ---
-# Convert batched predictions into one Pandas DataFrame again
-row_dicts = []
-for prediction in pred_output:
-    row_dicts.append({
-        x["label"]: x["score"] for x in prediction
-    })
+    # --- Loading Model & Inference ---
+    print(f"Loading model from {model_path}")
+    model = AutoModelForSequenceClassification.from_pretrained(model_path)
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    inference_pipeline = pipeline("text-classification", model=model, tokenizer=tokenizer, top_k=None,
+                                  device=args.device)
 
-inference_classified_unique_probs = pd.DataFrame.from_dict(row_dicts)
-inference_classified_unique_probs.index = inference_df.Input_String
+    print("Performing inference")
+    pred_output = []
+    for out in tqdm(inference_pipeline(KeyDataset(inference_dataset, "_Input_String"), batch_size=args.batch_size)):
+        pred_output.append(out)
 
-# Expand to original size and labels
-inference_classified_probs = inference_indication_cased.to_frame() \
-    .merge(inference_classified_unique_probs,
-           left_on="Indication",
-           right_index=True,
-           how="left",
-           validate="many_to_one")
+    # --- Postprocess Data ---
+    # Convert the predictions to a DataFrame
+    row_dicts = [{x["label"]: x["score"] for x in prediction} for prediction in pred_output]
+    inference_classified_unique_probs = pd.DataFrame.from_dict(row_dicts)
+    inference_classified_unique_probs.index = inference_dataset["_Input_String"]
 
-inference_classified_probs = inference_classified_probs.set_index("Indication")
+    # Binarise the predictions
+    inference_classified_unique_binarised = (inference_classified_unique_probs > args.pred_threshold) * 1
 
-# Binarise the output
-inference_classified_binarised = (inference_classified_probs > pred_threshold) * 1
+    # Expand back to the original size and order
+    inference_classified_binarised = input_df.merge(
+        inference_classified_unique_binarised, left_on="_Input_String", right_index=True, how="left",
+        validate="many_to_one")
 
-print(inference_classified_binarised)
+    inference_classified_binarised = (
+        inference_classified_binarised
+        .drop(columns=["_Input_String"])
+        .set_index(indication_column)
+    )
+
+    # Save results to output path
+    inference_classified_binarised.to_csv(output_file)
+    print(f"Results saved to {output_file}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Classify Free Text Indications")
+    parser.add_argument("--input_file", type=str, required=True, help="Path to the input files")
+    parser.add_argument("--output_file", type=str, required=True, help="Path to save the output results")
+    parser.add_argument("--model_path", type=str, required=True,
+                        help="Path to the directory containing the model and tokenizer")
+    parser.add_argument("--indication_column", default="Indication", type=str,
+                        help="Name of the column in the input file containing the indications")
+    # Optional Arguments
+    parser.add_argument("--unique_output", action="store_true", help="Returns the result for just the distinct subset of indications.")
+    parser.add_argument("--device", type=str, default="cpu",
+                        help="Device to run the model on ('cpu' or '0', '1',... for GPU)")
+    parser.add_argument("--batch_size", type=int, default=8, help="Batch size for inference")
+    parser.add_argument("--pred_threshold", type=float, default=0.5, help="Threshold for binarizing the predictions")
+
+    args = parser.parse_args()
+    main(args)
